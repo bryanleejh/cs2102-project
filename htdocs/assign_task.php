3d10dc923c81f7b4d50b374c611cba399aa6406a
Taha Tehseen <33021614+ttehseen@users.noreply.github.com>
2018-11-08 17:21:08 +0800

    Assign_task button + table query

--- htdocs/assign_task.php
+++ htdocs/assign_task.php
@@ -6,6 +6,15 @@
     <style type="text/css">
         body{ font: 14px sans-serif; }
         .wrapper{ width: 350px; padding: 20px; }
+
+          table {
+            width: 100%;
+            min-width: 500px;
+          }
+          th, td {
+            padding: 20px;
+            border: 1px solid #444444;
+          }
     </style>
 </head>
 <body>
@@ -13,82 +22,57 @@
         <?php include 'menu.php';?>
     </div>
     <div class="wrapper">
-        <h2>Assign Task</h2>
-    </div>
+        <h2> Assign Task</h2>
+        <?php
+        session_start();
+        $userid = $_SESSION['user'];
+        // echo "USER ID!!!: ";
+        // echo $userid;
 
-    <?php
-    session_start();
-    $userid = $_SESSION['user'];
-    // echo "USER ID!!!: ";
-    // echo $userid;
+        echo '<h5>' . 'Tasks To Assign' . '</h5>';
+        ob_start();
+        include 'login.php';
+        ob_end_clean();
 
-    $db     = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=test");
-        /*// random query with same output fields to test if table itself is displayed properly, cus assignment not implemented yet
-        $result = pg_query($db, "SELECT u.user_name, t.due_date, t.due_time, t.description FROM tasks t, users u WHERE t.task_id < 10 and t.owner_id = u.user_id");*/
-
-        $result = pg_query($db, "SELECT u.user_name, t.due_date, t.due_time, t.description FROM is_picked_for p, tasks t, users u WHERE '$row[user_id]' = p.bidder_id and p.task_id = t.task_id and t.owner_id = u.user_id");
+        $db     = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=test");
+        $result = pg_query($db, "SELECT u.user_name, t.description, t.due_date, t.due_time, b.amount FROM bids b, tasks t, users u WHERE b.bidder_id = u.user_id and b.task_id = t.task_id and t.owner_id = $userid and t.task_id NOT IN (SELECT p.task_id FROM is_picked_for p)");
 
         $i = 0;
         echo '<table><tr>';
         while ($i < pg_num_fields($result))
         {
             $fieldName = str_replace("_"," ",pg_field_name($result, $i));
-            $fieldName = str_replace("user","owner",$fieldName);
-            echo '<td>' . $fieldName . '</td>';
+            $fieldName = str_replace("user","bidder",$fieldName);
+            echo '<td>' . ucwords($fieldName) . '</td>';
             $i = $i + 1;
         }
+        echo '<td> Assign to this Bidder </td>';
         echo '</tr>';
 
         while ($row = pg_fetch_row($result)) 
         {
             echo '<tr>';
+            // echo '<button type="button">Assign</button>';
             $count = count($row);
             $y = 0;
             while ($y < $count)
             {
                 $c_row = current($row);
-                echo '<td>' . $c_row . '</td>';
+                echo '<td>' . $c_row . '</td>' ;
+                // . '<td><button>Sell</button><td>';
                 next($row);
                 $y = $y + 1;
             }
+            // echo '<td> <button type="button">Assign</button> </td>';
+            echo '<td><button type="submit" formaction="register.php">Assign</button></td>';
             echo '</tr>';
+            // echo "<form>";
+        // echo "<form action='tag.php' method='post'>";
+        // echo "<input type='submit' name='submit'>";
+        // echo "</form>";
         }
         pg_free_result($result);
         echo '</table>';
-
-        echo '<h5>' . 'Tasks Pending for Assignment' . '</h5>';
-
-        /*// random query with same output fields to test if table itself is displayed properly, cus assignment not implemented yet
-        $result = pg_query($db, "SELECT u.user_name, t.due_date, t.due_time, t.description, b.amount FROM bids b, tasks t, users u WHERE b.bidder_id = 4 and b.task_id = t.task_id and t.owner_id = u.user_id");*/
-
-        $result = pg_query($db, "SELECT u.user_name, t.due_date, t.due_time, t.description, b.amount FROM bids b, tasks t, users u WHERE '$row[user_id]' = b.bidder_id and b.task_id = t.task_id and t.owner_id = u.user_id and t.task_id NOT IN (SELECT p.task_id FROM is_picked_for p)");
-        $i = 0;
-        echo '<table><tr>';
-        while ($i < pg_num_fields($result))
-        {
-            $fieldName = str_replace("_"," ",pg_field_name($result, $i));
-            $fieldName = str_replace("user","owner",$fieldName);
-            echo '<td>' . $fieldName . '</td>';
-            $i = $i + 1;
-        }
-        echo '</tr>';
-
-        while ($row = pg_fetch_row($result)) 
-        {
-            echo '<tr>';
-            $count = count($row);
-            $y = 0;
-            while ($y < $count)
-            {
-                $c_row = current($row);
-                echo '<td>' . $c_row . '</td>';
-                next($row);
-                $y = $y + 1;
-            }
-            echo '</tr>';
-        }
-        pg_free_result($result);
-        echo '</table>';
-    ?>
+        ?>
 </body>
 </html>

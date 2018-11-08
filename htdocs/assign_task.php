21afb97800194389c2f068897fd69af2d2d524d8
Jasmine Tan <jasmine.tpm@u.yale-nus.edu.sg>
2018-11-09 00:40:18 +0800

    implemented assign_task

--- htdocs/assign_task.php
+++ htdocs/assign_task.php
@@ -51,7 +51,7 @@
         ob_end_clean();
 
         $db     = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=test");
-        $result = pg_query($db, "SELECT u.user_name, t.description, t.due_date, t.due_time, b.amount FROM bids b, tasks t, users u WHERE b.bidder_id = u.user_id and b.task_id = t.task_id and t.owner_id = $userid and t.task_id NOT IN (SELECT p.task_id FROM is_picked_for p)");
+        $result = pg_query($db, "SELECT u.user_id, u.user_name, t.task_id, t.description, t.due_date, t.due_time, b.amount FROM bids b, tasks t, users u WHERE b.bidder_id = u.user_id and b.task_id = t.task_id and t.owner_id = $userid and t.task_id NOT IN (SELECT p.task_id FROM is_picked_for p)");
 
         $i = 0;
         echo '<table><tr>';
@@ -65,7 +65,8 @@
         echo '<th> Assign to this Bidder </th>';
         echo '</tr>';
 
-        while ($row = pg_fetch_row($result)) 
+        $counter = 0;
+        while ($row = pg_fetch_row($result))
         {
             echo '<tr>';
             // echo '<button type="button">Assign</button>';
@@ -81,10 +82,11 @@
             }
             // echo '<td> <button type="button">Assign</button> </td>';
             echo "<td>";
-            echo "<form>"; 
-            echo "<form action='login.php' method='post'>";
-            echo "<input type='submit' name='submit' value='submit'>";
+            //echo "<form>"; 
+            echo "<form action='' method='post'>";
+            echo "<input type='submit' name='accept' value=$counter > ";
             echo "</form>";
+            $counter = $counter + 1;
             echo "</td>";
             // echo '<td><button type="submit" formaction="register.php">Assign</button></td>';
             echo '</tr>';
@@ -93,11 +95,26 @@
         // echo "<input type='submit' name='submit'>";
         // echo "</form>";
         }
-        
         pg_free_result($result);
+
+        if (isset($_POST['accept'])) {
+            $rownumber = $_POST['accept'];
+            $result = pg_query($db, "SELECT t.task_id, b.bid_id FROM bids b, tasks t, users u WHERE b.bidder_id = u.user_id and b.task_id = t.task_id and t.owner_id = $userid and t.task_id NOT IN (SELECT p.task_id FROM is_picked_for p) ORDER BY user_id LIMIT 1 OFFSET $rownumber;");
+            $bid = pg_fetch_assoc($result);
+            // echo $_POST['accept'];
+            $bidder = $bid['bid_id'];
+            $task = $bid['task_id'];
+            echo $bidder;
+            echo $task;
+            //$result = pg_query($db, "INSERT INTO is_picked_for (task_id, bid_id) VALUES ('$_POST[username]','$_POST[password]','$_POST[email]')");
+            $result = pg_query($db, "INSERT INTO is_picked_for(task_id, bid_id) VALUES ($task, $bidder);");
+            echo $result;
+            // $result = pg_query($db, "SELECT u.user_id, t.task_id FROM bids b, tasks t, users u WHERE b.bidder_id = u.user_id and b.task_id = t.task_id and t.owner_id = $userid and t.task_id NOT IN (SELECT p.task_id FROM is_picked_for p) ORDER BY user_id LIMIT 1 OFFSET $rownumber;");
+            // $results = pg_fetch_result($result);
+            //echo $result;
+          }
+
         echo '</table>';
-        if(isset($_POST['submit'])) {
-            echo "hello";}
         ?>
 </body>
 </html>
